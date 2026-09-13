bdd24d63ad1cf7861735abf6d9f08ff73ca038f7
manhtuan28 <buimanhtuan2k4@gmail.com>
2026-09-13 19:11:21 +0700

    fix(settings): restore selectedValue in LiveActions.m

--- Settings/LiveActions.m
+++ Settings/LiveActions.m
@@ -71,7 +71,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
+    NSNumber *selectedValue = self.liveFuncValues[indexPath.row];
     [[NSUserDefaults standardUserDefaults] setValue:selectedValue forKey:@"live_action"];
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"en_livefunc"];
     [[NSUserDefaults standardUserDefaults] synchronize];
